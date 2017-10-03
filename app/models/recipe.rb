9f170a79e24e63af07a966c9bb5ba9d5e202209f
Joao Almeida <joaorsalmeida@gmail.com>
2017-10-02 22:13:20 -0300

    Register recipe with cuisine

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  belongs_to :cuisine
+
   def show_cook_time
     "#{cook_time} minutos"
   end
--- spec/features/visitor_register_recipe_spec.rb
+++ spec/features/visitor_register_recipe_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 feature 'Visitor register recipe' do
   scenario 'successfully' do
     #cria os dados necessários, nesse caso não vamos criar dados no banco
-
+    Cuisine.create(name: 'Arabe')
     # simula a ação do usuário
     visit root_path
     click_on 'Enviar uma receita'
 
     fill_in 'Título', with: 'Tabule'
     fill_in 'Tipo da Receita', with: 'Entrada'
-    fill_in 'Cozinha', with: 'Arabe'
+    select 'Arabe', from: 'Cozinha'
     fill_in 'Dificuldade', with: 'Fácil'
     fill_in 'Tempo de Preparo', with: '45'
     fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
     fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
-    click_on 'Enviar'
+    click_on 'Enviar' # GET /recipes
 
-    #expectativas do usuário
+
     expect(page).to have_css('h1', text: 'Tabule')
     expect(page).to have_css('h3', text: 'Detalhes')
     expect(page).to have_css('p', text: 'Entrada')
